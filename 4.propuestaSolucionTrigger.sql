b6362b9f5e9f2be1fb37f9e253dc1103e0619902
DamianUS <damiancerero@us.es>
2025-01-06 11:44:33 +0100

    Cambios menores en la solución propuesta

--- 4.propuestaSolucionTrigger.sql
+++ 4.propuestaSolucionTrigger.sql
@@ -4,27 +4,26 @@ CREATE TRIGGER t_asegurar_mismo_tipo_producto_en_pedidos
 BEFORE INSERT ON LineasPedido
 FOR EACH ROW
 BEGIN
-    DECLARE v_tipoProductoNuevo VARCHAR(255);
-    DECLARE v_tipoProductoExistente VARCHAR(255);
+    DECLARE v_idTipoProductoNuevo INT;
+    DECLARE v_existeOtroTipoProductoEnPedido BOOLEAN;
 
     -- Obtener el tipo del producto que se intenta insertar
-    SELECT tp.nombre
-    INTO v_tipoProductoNuevo
+    SELECT Productos.tipoProductoId
+    INTO v_idTipoProductoNuevo
     FROM Productos p
-    JOIN TiposProducto tp ON p.tipoProductoId = tp.id
     WHERE p.id = NEW.productoId;
 
     -- Verificar si el pedido ya tiene líneas y qué tipo de producto contiene
-    SELECT tp.nombre
-    INTO v_tipoProductoExistente
-    FROM LineasPedido lp
-    JOIN Productos p ON lp.productoId = p.id
-    JOIN TiposProducto tp ON p.tipoProductoId = tp.id
-    WHERE lp.pedidoId = NEW.pedidoId
-    LIMIT 1;
+    SELECT EXISTS (
+        SELECT *
+        FROM LineasPedido lp
+        JOIN Productos p ON lp.productoId = p.id
+        WHERE lp.pedidoId = NEW.pedidoId
+          AND p.tipoProductoId <> v_idTipoProductoNuevo
+    ) INTO v_existeOtroTipoProductoEnPedido;
 
     -- Si el pedido ya tiene líneas, comparar los tipos y lanzar excepción si son diferentes
-    IF v_tipoProductoExistente IS NOT NULL AND v_tipoProductoExistente <> v_tipoProductoNuevo THEN
+    IF v_existeOtroTipoProductoEnPedido THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'El pedido no puede incluir productos de tipos diferentes (físicos y digitales).';
     END IF;
